8e817d58dcd344ac8d74ed229e32ed5592f14507
Madang Garden <chooi9522@gmail.com>
2021-03-08 21:12:43 +0900

    #0.4 First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hi");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Lemma", race = "midlander", job = "black mage", level = 80;
+const sayHi = (name, race, job, level) => {
+    console.log(`Hello ${name}, you are ${level} level ${job}, you are a ${race} `);
+};
+sayHi(name, race, job, level);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hi");
+const name = "Lemma",
+    race = "midlander",
+    job = "black mage",
+    level = 80;
+
+const sayHi = (name, race, job, level) => {
+    console.log(`Hello ${name}, you are ${level} level ${job}, you are a ${race} `);
+}
+
+sayHi(name, race, job, level);
+
+export {};
