e36e95f5b90a091548d5d2ab6b0ea04c0e625c74
Madang Garden <chooi9522@gmail.com>
2021-03-13 23:51:30 +0900

    #0.6 Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const name = "Lemma",
     job = "black mage",
     level = 80;
 
-const sayHi = (name, race, job, level) => {
+const sayHi = (name: string, race: string, job: string, level:number): void => {
     console.log(`Hello ${name}, you are ${level} level ${job}, you are a ${race} `);
 }
 
